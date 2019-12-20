c6de86a0d73cfda9c51cf83e8752aaa5911bd136
“Mark <mkanko66@gmail.com>
2019-12-20 11:13:44 -0800

    lab completed

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,7 +1,7 @@
 export function managePresents(state = {numberOfPresents: 0}, action){
     switch (action.type) {
         case 'INCREASE':
-            return state + 1
+            return {numberOfPresents: state.numberOfPresents + 1}
         default:
             return state  
     }
