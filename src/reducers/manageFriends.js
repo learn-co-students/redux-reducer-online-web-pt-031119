b590fb8e0191676926f85d241fabcac10a733126
Ryan <kerneltux0@gmail.com>
2020-01-07 11:57:24 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case 'ADD_FRIEND':
+      state.friends.push(action.friend)
+      return state
+    case 'REMOVE_FRIEND':
+      let friendsArray = []
+      for(var i=0;i<state.friends.length;i++)
+        if(state.friends[i].id != action.id){
+          friendsArray.push(state.friends[i]);
+        }
+      return {friends: friendsArray}
+    default:
+      return state
+  }
 }
